5aad582d34393dc06bd4eed9c4e16a27fb24fa66
Sardius <0xsardius@gmail.com>
2023-04-15 18:36:08 -0400

    Finished initial booklist components.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,34 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css";
 
-function Greeting() {
-  return <h1>gm</h1>;
-}
+const BookList = () => {
+  return (
+    <section>
+      <Book />
+    </section>
+  );
+};
 
+const Book = () => {
+  return (
+    <article>
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  );
+};
+
+const Image = () => (
+  <img
+    src="https://m.media-amazon.com/images/I/51YGx8qVVXL.jpg"
+    alt="Hello Beautiful Novel Cover"
+  />
+);
+const Title = () => <h2>Hello Beautiful</h2>;
+const Author = () => {
+  return <h4>Ann Napolitano</h4>;
+};
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Greeting />);
+root.render(<BookList />);
