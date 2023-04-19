f07f23fe5fee8a0f53fae931e91c07b54fb557f9
Sardius <0xsardius@gmail.com>
2023-04-18 20:20:11 -0400

    Updated index.js

--- src/index.js
+++ src/index.js
@@ -2,21 +2,39 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
+const firstBook = {
+  title: "Hello Beautiful",
+  author: "Ann Napolitano",
+  img: "./images/book-cover-1.jpg",
+};
+const secondBook = {
+  title: "Dog Man: Twenty Thousand Fleas Under the Sea",
+  author: "Dav Pilkey",
+  img: "https://m.media-amazon.com/images/I/51VyW1qRmSL._SX329_BO1,204,203,200_.jpg",
+};
+
 const BookList = () => {
   return (
     <section className="booklist">
-      <Book />
+      <Book
+        author={firstBook.author}
+        title={firstBook.title}
+        img={firstBook.img}
+      />
+      <Book
+        author={secondBook.author}
+        title={secondBook.title}
+        img={secondBook.img}
+      />
     </section>
   );
 };
 
-const Book = () => {
-  const title = "Hello Beautiful";
-  const author = "Ann Napolitano";
-
+const Book = (props) => {
+  const { img, title, author } = props;
   return (
     <article className="book">
-      <img src="./images/book-cover-1.jpg" alt="Hello Beautiful Novel Cover" />
+      <img src={img} alt={title} />
       <h2>{title}</h2>
       <h4>{author.toUpperCase()}</h4>
     </article>
