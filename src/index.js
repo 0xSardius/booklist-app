cd4eaa00f9bdef3fb90084cbbfbcc74d23f98067
Sardius <0xsardius@gmail.com>
2023-05-06 07:37:53 -0400

    Updating index.js

--- src/index.js
+++ src/index.js
@@ -2,30 +2,26 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
-const firstBook = {
-  title: "Hello Beautiful",
-  author: "Ann Napolitano",
-  img: "./images/book-cover-1.jpg",
-};
-const secondBook = {
-  title: "Dog Man: Twenty Thousand Fleas Under the Sea",
-  author: "Dav Pilkey",
-  img: "https://m.media-amazon.com/images/I/51VyW1qRmSL._SX329_BO1,204,203,200_.jpg",
-};
+const books = [
+  {
+    title: "Hello Beautiful",
+    author: "Ann Napolitano",
+    img: "./images/book-cover-1.jpg",
+  },
+  {
+    title: "Dog Man: Twenty Thousand Fleas Under the Sea",
+    author: "Dav Pilkey",
+    img: "https://m.media-amazon.com/images/I/51VyW1qRmSL._SX329_BO1,204,203,200_.jpg",
+  },
+];
 
 const BookList = () => {
   return (
     <section className="booklist">
-      <Book
-        author={firstBook.author}
-        title={firstBook.title}
-        img={firstBook.img}
-      />
-      <Book
-        author={secondBook.author}
-        title={secondBook.title}
-        img={secondBook.img}
-      />
+      {books.map((book) => {
+        const { img, title, author } = book;
+        return <Book img={img} title={title} author={author} />;
+      })}
     </section>
   );
 };
