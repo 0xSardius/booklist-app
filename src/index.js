c7c8a7521928350329579aad809e10ffeeda20a2
Sardius <0xsardius@gmail.com>
2023-05-06 08:01:03 -0400

    Refactoring with spread operator

--- src/index.js
+++ src/index.js
@@ -7,27 +7,29 @@ const books = [
     title: "Hello Beautiful",
     author: "Ann Napolitano",
     img: "./images/book-cover-1.jpg",
+    id: 1,
   },
   {
     title: "Dog Man: Twenty Thousand Fleas Under the Sea",
     author: "Dav Pilkey",
     img: "https://m.media-amazon.com/images/I/51VyW1qRmSL._SX329_BO1,204,203,200_.jpg",
+    id: 2,
   },
 ];
 
 const BookList = () => {
   return (
     <section className="booklist">
-      {books.map((book) => {
-        const { img, title, author } = book;
-        return <Book img={img} title={title} author={author} />;
+      {books.map((book, index) => {
+        const { img, title, author, id } = book;
+
+        return <Book {...book} img={img} key={book.id} />;
       })}
     </section>
   );
 };
 
-const Book = (props) => {
-  const { img, title, author } = props;
+const Book = ({ book: { img, title, author } }) => {
   return (
     <article className="book">
       <img src={img} alt={title} />
